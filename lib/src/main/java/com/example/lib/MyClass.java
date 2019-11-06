e29995c265b8aec37123d8978cac39bd767ea29a
ppkk8855 <ppkk8855@gmail.com>
2019-11-06 15:30:13 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,14 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class MyClass {
+
+    public static void main ( String[] args ) {
+
+            System.out.println("hello world+3A513130");
+        }
+
+
+
+}
